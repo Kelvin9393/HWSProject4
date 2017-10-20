d198a34c642a080c4f369e03b5ea7eac6deafa9a
Kelvin9393 <kelvinlssiang9393@hotmail.com>
2017-10-20 10:00:45 +0800

    modified link

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -57,7 +57,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     
     func openPage(action: UIAlertAction) {
-        let url = URL(string: "https://\(websites[0])")!
+        let url = URL(string: "https://\(action.title!)")!
         webView.load(URLRequest(url: url))
     }
     
